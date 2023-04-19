a707220e00766c325e80242a0dc9a559b4b23556
Suleymanov Denis <kingmidas1992@gmail.com>
2023-04-19 15:04:43 +0400

    Default layout

---
+++ src/modules/layout/layout.component.ts
@@ -0,0 +1,11 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'cg-layout',
+  templateUrl: './layout.component.html',
+  encapsulation: ViewEncapsulation.None,
+  styleUrls: ['./layout.component.scss']
+})
+export class LayoutComponent {
+
+}
