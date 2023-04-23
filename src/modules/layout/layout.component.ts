8157cd62aabe0dbb5b740f07b683e8b8990c347a
Suleymanov Denis <kingmidas1992@gmail.com>
2023-04-23 21:34:41 +0400

    add mixins

--- src/modules/layout/layout.component.ts
+++ src/modules/layout/layout.component.ts
@@ -3,7 +3,6 @@ import { Component, Input, ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'cg-layout',
   templateUrl: './layout.component.html',
-  encapsulation: ViewEncapsulation.None,
   styleUrls: ['./layout.component.scss']
 })
 export class LayoutComponent {
